3cafcfaba8eefd2152bee5acaf0fed3c1dc733f0
mao <mao@bassdrum.org>
2023-03-04 04:26:03 +0900

    temporarily delete all files.

--- usagiMultiBuild/Source/usagiMultiBuild/usagiMultiBuild.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "usagiMultiBuild.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, usagiMultiBuild, "usagiMultiBuild" );
- 
