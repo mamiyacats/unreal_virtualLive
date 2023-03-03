e07cccf48880136eb89f369fc8acac5795e91678
mao <mao@bassdrum.org>
2023-03-04 04:35:20 +0900

    return to the original.

---
+++ usagiMultiBuild/Source/usagiMultiBuild/usagiMultiBuild.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "usagiMultiBuild.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, usagiMultiBuild, "usagiMultiBuild" );
+ 
---
+++ usagiMultiBuild/Source/usagiMultiBuild/usagiMultiBuildGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "usagiMultiBuildGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AusagiMultiBuildGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AusagiMultiBuildGameMode();
+};
+
+
+
